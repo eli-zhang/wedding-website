552ef45daafcb8efdcf7326067644b8af4d19a03
Eli <zeliboid8@gmail.com>
2026-01-19 15:00:19 -0500

    Update animations to use non-rotated images

--- src/pages/InvitationPage/styled.js
+++ src/pages/InvitationPage/styled.js
@@ -1,39 +1,37 @@
 import styled, { keyframes } from 'styled-components';
 import cardInvitation from '../../images/invitation/card_invitation.png';
-import inviteRotated from '../../images/invitation/invite_rotated.png';
 import envelopeBottom from '../../images/invitation/envelope_bottom.png';
 import envelopeTop from '../../images/invitation/envelope_top.png';
 
 import paperTexture from '../../images/paper_texture.jpg';
-import { accentColorGreen } from '../../constants';
 
 const slideIn = keyframes`
     0% {
-        transform: translateX(-110%) rotate(-10deg);
+        transform: translateX(-110%) rotate(25deg);
     }
     100% {
-        transform: translateX(0) rotate(0deg);
+        transform: translateX(0) rotate(15deg);
     }
 `;
 
 const openEnvelope = keyframes`
     from {
-        transform: translate(0, 0);
+        transform: translate(0, 0) rotate(15deg);
     }
     to {
-        transform: translate(-10vw, ${20 / 0.26794919243}vh);
+        transform: translate(-10vw, ${20 / 0.26794919243}vh) rotate(15deg);
     }
 `;
 
 const openCard = keyframes`
     0% {
-        transform: translate(0, 0);
+        transform: translate(0, 0) rotate(15deg);
     }
     60% {
-        transform: translate(${2.5 / 4}vw, -${5 / 4 / 0.26794919243}vh) rotate(0deg);
+        transform: translate(${5 / 4}vw, -${5 / 4 / 0.26794919243}vh) rotate(15deg);
     }
     100% {
-        transform: translate(${2.5 / 4}vw, -${5 / 4 / 0.26794919243}vh) rotate(-15deg);
+        transform: translate(${5 / 4}vw, -${5 / 4 / 0.26794919243}vh) rotate(0deg);
     }
 `;
 
@@ -67,13 +65,19 @@ export const InvitationCard = styled.div`
     position: absolute;
     width: 79vw;
     height: 79vh;
-    background-image: url(${inviteRotated});
+    background-image: url(${cardInvitation});
     background-repeat: no-repeat;
     background-size: contain;
     background-position: center center;
     animation: ${props => props.$isOpened ? openCard : slideIn} ${props => props.$isOpened ? '3s' : '1.5s'} cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards;;
     z-index: 2;
     transition: transform 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+    &:hover {
+        transform: ${props => props.$isOpened
+        ? `translate(${5 / 4}vw, -${5 / 4 / 0.26794919243}vh) rotate(15deg) scale(1.15)`
+        : 'rotate(15deg) scale(1.15)'};
+    }
 `;
 
 
@@ -87,6 +91,13 @@ export const EnvelopeBottom = styled.div`
     background-position: center center;
     animation: ${props => props.$isOpened ? openEnvelope : slideIn} 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards;;
     z-index: 1;
+    transition: transform 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+    &:hover {
+        transform: ${props => props.$isOpened
+        ? `translate(-10vw, ${20 / 0.26794919243}vh) rotate(15deg) scale(1.15)`
+        : 'scale(1.15)'};
+    }
 `;
 
 export const EnvelopeTop = styled.div`
@@ -99,6 +110,13 @@ export const EnvelopeTop = styled.div`
     background-position: center center;
     animation: ${props => props.$isOpened ? openEnvelope : slideIn} 1.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards;;
     z-index: 3;
+    transition: transform 0.4s cubic-bezier(0.165, 0.84, 0.44, 1);
+
+    &:hover {
+        transform: ${props => props.$isOpened
+        ? `translate(-10vw, ${20 / 0.26794919243}vh) rotate(15deg) scale(1.15)`
+        : 'scale(1.15)'};
+    }
 `;
 
 
@@ -118,7 +136,7 @@ export const NavigationArrow = styled.div`
     transition: transform 0.3s ease, color 0.3s ease;
 
     &:hover {
-        transform: translateY(-50%) translateX(5px);
+        transform: translateY(-50%) translateX(5px) scale(1.15);
         color: #000;
     }
 
